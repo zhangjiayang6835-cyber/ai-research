c23ed3897a37cff562311df6802d722290179134
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-07-02 01:32:20 -0400

    fix: apply solution for issue #268

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .session_cookie import SessionCookieManager, create_session_manager, SessionManager
+
+__all__ = ['SessionCookieManager', 'create_session_manager', 'SessionManager']
+
+__version__ = '1.0.0'
